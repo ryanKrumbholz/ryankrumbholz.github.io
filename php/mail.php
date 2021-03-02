f43757ee62d57fd95d1a749b41f40a60f9485403
ryanKrumbholz <krumbholz98@gmail.com>
2021-03-02 11:48:00 -0800

    .

--- php/mail.php
+++ php/mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "hello@sojonever.com";
+        $recipient = "krumbholz98@gmail.com";
 
         // Set the email subject.
         $subject = "New Message from niz by - $name";
